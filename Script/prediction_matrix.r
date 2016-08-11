0dd358f6d8a140dd842248c875d7a97506bed49d
David Beauchesne <davidbeauchesne@Davids-MacBook-Pro.local>
2016-08-11 18:03:23 -0400

    Added the option to consider only empirical data in the predictions, hence considering the contribution of the interaction catalog

--- Script/prediction_matrix.r
+++ Script/prediction_matrix.r
@@ -1,5 +1,8 @@
 # Extracting as interaction matrix
-prediction_matrix <- function(S1, predictions, predict.only) {
+prediction_matrix <- function(S1, predictions, predict.only = FALSE, empirical.only = FALSE) {
+    # predict.only: only predictions will be used to recreate the prediction matrix
+    # empirical.only: only empirical data will be used to recreate the prediction matrix, corresponding to the contribution of the interaction catalog
+    # rownames need to be taxa names
     predict.matrix <- matrix(nrow = length(S1), ncol = length(S1), data = 0, dimnames = list(S1,S1))
 
     for(i in 1:length(S1)) {
@@ -10,7 +13,11 @@ prediction_matrix <- function(S1, predictions, predict.only) {
           NULL
         } else {
             for(j in 1:length(predict)) {
-                predict.matrix[predict[j],i] <- 1
+                if(empirical.only == TRUE) {
+                    break
+                } else { # empirical.only == FALSE
+                    predict.matrix[predict[j],i] <- 1
+                } #if
             }#j
         }#if
 
@@ -20,7 +27,7 @@ prediction_matrix <- function(S1, predictions, predict.only) {
             for(j in 1:length(empirical)) {
                 if(predict.only == TRUE) {
                     break
-                } else {
+                } else { # predict.only == FALSE
                     predict.matrix[empirical[j],i] <- 1
                 } #if
             }#j
