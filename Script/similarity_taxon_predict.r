6909a0c10dc1155799f2b1631eab1ac98be46622
David Beauchesne <david.beauchesne@uqar.ca>
2016-08-12 16:02:03 -0400

    add evaluation of similarity between resources based on set of consumers

--- Script/similarity_taxon_predict.r
+++ Script/similarity_taxon_predict.r
@@ -1,11 +1,13 @@
 #Similarity matrix as a single functions
-similarity_taxon_predict <- function(S0, S1, wt, similarity.matrix) {
+similarity_taxon_predict <- function(S0, S1, wt, similarity.matrix, taxa) {
 
     # Parameters:
     #   S0                  A large set of species and their preys, with column structure ['taxon', 'taxonomy', 'resource', 'non-resource'] (if blind = TRUE, it )
     #   S1                  The subset of S0 where we want to predict new preys, string vector
     #   wt                  Weight given to traits (including phylogeny) when computing similarities.
     #   similarity.matrix   Similarity.matrix for interaction catalogue (measured by function: similarity_taxon)
+    #   taxa                Either resource or consumer
+
     #
     # Output
     #   A matrix of dimensions S x S with similarity measurements for all combinations of S[i,j]
@@ -16,22 +18,26 @@ similarity_taxon_predict <- function(S0, S1, wt, similarity.matrix) {
     similarity.matrix[, to.recalculate] <- similarity.matrix[to.recalculate, ] <- 0
 
     taxonomy <- vector("list",nrow(S0))
-    resource <- vector("list",nrow(S0))
+    taxon <- vector("list",nrow(S0))
     for(i in 1:nrow(S0)) {
       taxonomy[[i]] <- unlist(strsplit(S0[i, 'taxonomy'], " \\|\\ "))
       if(length(which(taxonomy[[i]] == "NA")) > 0) {
           taxonomy[[i]] <- taxonomy[[i]][-which(taxonomy[[i]] == "NA")]
       }
-      resource[[i]] <- unlist(strsplit(S0[i, 'resource'], " \\|\\ "))
-    }
 
+        if(taxa == 'consumer') {
+            taxon[[i]] <- unlist(strsplit(S0[i, 'resource'], " \\|\\ "))
+        } else if(taxa == 'resource') {
+            taxon[[i]] <- unlist(strsplit(S0[i, 'consumer'], " \\|\\ "))
+        }
+    }
 
   # pb <- txtProgressBar(min = (nrow(similarity.matrix) - length(taxa.add)), max = ncol(similarity.matrix), style = 3)
 
   for(i in to.recalculate){
       for(j in 1:ncol(similarity.matrix)){ #No need to evaluate both side of the matrix diagonal for the similarity matrix
-          similarity.matrix[i,j] <- similarity.matrix[j,i] <- tanimoto_traits(resource_x = resource[[i]],
-                                                                              resource_y = resource[[j]],
+          similarity.matrix[i,j] <- similarity.matrix[j,i] <- tanimoto_traits(resource_x = taxon[[i]],
+                                                                              resource_y = taxon[[j]],
                                                                               trait_x = taxonomy[[i]],
                                                                               trait_y = taxonomy[[j]],
                                                                               trait_weight = wt
