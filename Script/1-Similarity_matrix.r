955f79a7fc6c9ef0aa3533495bb7baa6615b5250
David Beauchesne <david.beauchesne@uqar.ca>
2016-08-12 16:02:03 -0400

    add evaluation of similarity between resources based on set of consumers

--- Script/1-Similarity_matrix.r
+++ Script/1-Similarity_matrix.r
@@ -22,14 +22,42 @@
 # -----------------------------------------------------------------------------
 # SCRIPT
 # -----------------------------------------------------------------------------
+load("./RData/Tanimoto_data.RData")
 
 # Measuring the similarity with multiple weights for all taxa in interaction catalogue
 # Will be better once we code for proximity graphs
-    wt <- seq(0, 1, by = 0.1)
-    similarity.matrices <- vector('list',11)
-    names(similarity.matrices) <- seq(0, 1, by = 0.1)
-    for(i in 1:length(wt)) {
-        similarity.matrices[[i]] <- similarity_taxon(S0 = S0_catalog, wt = wt[i])
-        save(x = similarity.matrices, file = "./RData/Similarity.matrices.RData")
+
+# S0: A large set of species and their preys, with column structure ['taxon', 'taxonomy', 'resource', 'non-resource', 'consumer', 'non-consumer']
+# Format interaction catalogue to fit this table format
+    S0_catalog <- matrix(nrow = nrow(Tanimoto_data[[1]]), ncol = 6, data = "", dimnames = list(Tanimoto_data[[1]][, 'taxon'], c('taxon', 'taxonomy', 'resource', 'non-resource', 'consumer', 'non-consumer')))
+    S0_catalog[, 1] <- Tanimoto_data[[1]][, 'taxon']
+    S0_catalog[, 2] <- Tanimoto_data[[1]][, 'kingdom | phylum | class | order | family | genus | species']
+    # From binary interactions catalogue with consumer, resources, interaction or non-interaction
+    for(k in 1:nrow(Tanimoto_data[[3]])) {
+        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 3] <- Tanimoto_data[[3]][k, 'resource']
+        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 4] <- Tanimoto_data[[3]][k, 'non-resource']
+        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 5] <- Tanimoto_data[[6]][k, 'consumer']
+        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 6] <- Tanimoto_data[[6]][k, 'non-consumer']
+
     }
-    save(x = similarity.matrices, file = "./RData/Similarity.matrices.RData")
+
+# Weight values for 2-way similarity measurements
+    wt <- seq(0, 1, by = 0.1)
+
+# 1st is for similarity measured from set of resources and taxonomy, for consumers
+    similarity.consumers <- vector('list',11)
+    names(similarity.consumers) <- wt
+    for(i in 1:length(wt)) {
+        similarity.consumers[[i]] <- similarity_taxon(S0 = S0_catalog, wt = wt[i], taxa = 'consumer')
+        save(x = similarity.consumers, file = "./RData/Similarity_consumers.RData")
+    }
+    save(x = similarity.consumers, file = "./RData/Similarity_consumers.RData")
+
+# 2nd is for similarity measured from set of consumers and taxonomy, for resources
+    similarity.resources <- vector('list',11)
+    names(similarity.resources) <- wt
+    for(i in 1:length(wt)) {
+        similarity.resources[[i]] <- similarity_taxon(S0 = S0_catalog, wt = wt[i], taxa = 'resource')
+        save(x = similarity.resources, file = "./RData/Similarity_resources.RData")
+    }
+    save(x = similarity.resources, file = "./RData/Similarity_resources.RData")
