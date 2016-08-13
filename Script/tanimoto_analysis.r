e929a7fe7dcddf5cd5df179145c360fef6bdaa1c
David Beauchesne <david.beauchesne@uqar.ca>
2016-08-13 10:59:49 -0400

    remove unnecessary command line

--- Script/tanimoto_analysis.r
+++ Script/tanimoto_analysis.r
@@ -71,7 +71,7 @@ tanimoto_analysis <- function(min.tx, K.values, MW, WT, minimum_threshold, simil
         names(communities) <- Cm
 
     # Setting up lists to store the results
-        wt.init <- wt.init <- c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+        wt.init <- c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
         wt.remove <- which(!wt.init %in% WT)# selecting a subset of similarity matrices
         for(i in rev(wt.remove)) {
             if(similarity == 'both') { # For similarity matrices already evaluated
