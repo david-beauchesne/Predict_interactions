19da5b4ac0e39412055dae24e68f7dfb1d9cef71
David Beauchesne <david.beauchesne@uqar.ca>
2016-09-16 14:13:18 -0400

    change call for catalog

--- Script/1-Similarity_matrix.r
+++ Script/1-Similarity_matrix.r
@@ -22,25 +22,7 @@
 # -----------------------------------------------------------------------------
 # SCRIPT
 # -----------------------------------------------------------------------------
-load("./RData/Tanimoto_data.RData")
-
-# Measuring the similarity with multiple weights for all taxa in interaction catalogue
-# Will be better once we code for proximity graphs
-
-# S0: A large set of species and their preys, with column structure ['taxon', 'taxonomy', 'resource', 'non-resource', 'consumer', 'non-consumer']
-# Format interaction catalogue to fit this table format
-    S0_catalog <- matrix(nrow = nrow(Tanimoto_data[[1]]), ncol = 6, data = "", dimnames = list(Tanimoto_data[[1]][, 'taxon'], c('taxon', 'taxonomy', 'resource', 'non-resource', 'consumer', 'non-consumer')))
-    S0_catalog[, 1] <- Tanimoto_data[[1]][, 'taxon']
-    S0_catalog[, 2] <- Tanimoto_data[[1]][, 'kingdom | phylum | class | order | family | genus | species']
-    # From binary interactions catalogue with consumer, resources, interaction or non-interaction
-    for(k in 1:nrow(Tanimoto_data[[3]])) {
-        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 3] <- Tanimoto_data[[3]][k, 'resource']
-        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 4] <- Tanimoto_data[[3]][k, 'non-resource']
-        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 5] <- Tanimoto_data[[6]][k, 'consumer']
-        S0_catalog[Tanimoto_data[[3]][k, 'consumer'], 6] <- Tanimoto_data[[6]][k, 'non-consumer']
-
-    }
-
+load("./RData/S0_catalog.RData")
 # Weight values for 2-way similarity measurements
     wt <- seq(0, 1, by = 0.1)
 
